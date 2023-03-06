943ce6f56303ea03a767d237a737ea5664a5e721
muratcanyalin <muratcanyalin07@gmail.com>
2023-03-06 16:06:12 +0300

    move lib

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      App is Running
+    </div>
+  )
+}
+
+export default App
---
+++ server/index.js
@@ -0,0 +1,46 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import dotenv from "dotenv"
+import cors from "cors";
+import postRoutes from "./routes/posts.js";
+
+
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+
+
+app.get("/", (req, res) => {
+    res.json({
+        author: "Coding with muratcan :)",
+        message: "Mutlu Yıllar! :) ",
+    });
+});
+
+
+
+app.use("/posts", postRoutes);
+
+const PORT = process.env.PORT || 5000;
+ 
+
+
+mongoose.connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}) 
+.then(() => {
+    app.listen(PORT, () => {
+        console.log("Server started on port " + PORT);
+
+    });
+})
+.catch((error) => {
+   console.error(error.message);
+})
